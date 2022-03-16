e92f2e7109e6a39aaeb70129b80f457bdfefe1bf
Sheng-Yuan <shengyuan13@gmail.com>
2022-03-15 22:03:32 -0600

    LTV-UI-02-Top-100-list: Added component to display top 100 urls

--- src/components/inputShortener.jsx
+++ src/components/inputShortener.jsx
@@ -1,32 +1,34 @@
 import React, { useState } from "react";
 
-function InputShortener({ setInputValue }) {
-    const [value, setValue] = useState("");
+function InputShortener({ setInputValue, setDisplayList }) {
+  const [value, setValue] = useState("");
 
-    // On shorten button click, update the inputValue.
-    const handleClick = () => {
-        setInputValue(value);
-        setValue("");
-    }
-    
-    return (
-      <div className="inputContainer">
-        <h1>
-         URL's <span>Shortener</span>
-        </h1>
-        <div className="input-group mb-3">
-          <input
-            className="input-group-text"
-            type="text"
-            placeholder="Paste a link to shorten it"
-            value={value}
-            onChange={(e)=> setValue(e.target.value)}
-          />
-          <button className="btn btn-success" onClick={handleClick}>shorten</button>
-        </div>
+  // On shorten button click, update the inputValue.
+  const handleClick = () => {
+    setInputValue(value);
+    setDisplayList(false);
+    setValue("");
+  };
+
+  return (
+    <div className="inputContainer">
+      <h1>
+        URL's <span>Shortener</span>
+      </h1>
+      <div className="input-group mb-3">
+        <input
+          className="input-group-text"
+          type="text"
+          placeholder="Paste a link to shorten it"
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+        />
+        <button className="btn btn-success" onClick={handleClick}>
+          shorten
+        </button>
       </div>
-    );
-  
+    </div>
+  );
 }
 
 export default InputShortener;
